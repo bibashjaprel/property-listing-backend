cee25c63b3b32fc0ebf59333fc7a4c327cdae689
Bibash Japrel <bibashjaprel111@gmail.com>
2025-05-31 21:22:08 +0545

    user profile setup

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -70,3 +70,31 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ error: 'Login failed' })
   }
 }
+
+
+export const userProfile = async (req, res) => {
+  try {
+    const userId = req.user.userId
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        profile_image: true,
+        role: true,
+        is_verified: true,
+      },
+    })
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    res.json({ user })
+  } catch (error) {
+    console.error('Error fetching user profile:', error)
+    res.status(500).json({ error: 'Failed to fetch user profile' })
+  }
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { authenticate } from "../middlewares/auth.middleware.js";
 const router = Router();
 import { createUser } from "../controllers/user.controller.js";
 import { loginUser } from "../controllers/user.controller.js";
+import { userProfile } from "../controllers/user.controller.js";
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
+router.get("/profile", authenticate, userProfile);
 
 
 export default router;
